0b9e7e7e90d0d01451ee94d3f9d25dc121df8e6d
Ivan Zrvnar <w0778779@myscc.ca>
2022-10-17 20:30:04 -0400

    Refactored Program Launch * refactored the main method * added screen dimensions * changed title

--- src/main/java/com/example/logbook/Main.java
+++ src/main/java/com/example/logbook/Main.java
@@ -1,18 +1,18 @@
 package com.example.logbook;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1024, 720);
+        stage.setTitle("Logbook");
         stage.setScene(scene);
         stage.show();
     }
