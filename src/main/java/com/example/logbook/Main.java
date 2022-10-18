05ff1d0435260b475510d7c864e65fa5a7911908
Ivan Zrvnar <w0778779@myscc.ca>
2022-10-17 21:18:28 -0400

    Created Crud text labels * Added CRUD text labels that include * Create workout, delete and update

--- src/main/java/com/example/logbook/Main.java
+++ src/main/java/com/example/logbook/Main.java
@@ -2,12 +2,17 @@ package com.example.logbook;
 
 import com.example.logbook.tabs.*;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -31,11 +36,35 @@ public class Main extends Application {
         tabPane.getTabs().addAll(new WorkoutTab(), new ExercisesTab(), new ProgressTab(), new RoutineBreakdownTab(), new WeightTrackerTab(), new SettingsTab());
         // setting the tabs to not be able to be closed
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        // Crud Labels
+        VBox crudLabels = new VBox();
+        crudLabels.setPadding(new Insets(10));
+        crudLabels.setSpacing(25);
+
+        // create workout text
+        Text createWorkoutText = new Text("Create Workout");
+        createWorkoutText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+
+
+        // Delete workout Text
+        Text deleteWorkoutText = new Text("Delete Workout");
+        deleteWorkoutText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+
+        //Update workout Text
+        Text updateWorkoutText = new Text("Update Workout");
+        updateWorkoutText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+
+
+        // Text added to vbox
+        crudLabels.getChildren().addAll(createWorkoutText, deleteWorkoutText, updateWorkoutText);
+
+
+        // Root additions
         root.setCenter(tabPane);
-
-
-
         root.setTop(menu);
+        root.setBottom(crudLabels);
+        //Scene
         Scene scene = new Scene(root, 1024, 768);
         stage.setTitle("Logbook");
         stage.setScene(scene);
